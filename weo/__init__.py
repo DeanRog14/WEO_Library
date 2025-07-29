1f6620a55ff32f90f456c54ef791ccd3843445a2
DeanRog14 <123507650+DeanRog14@users.noreply.github.com>
2025-07-29 10:49:42 -0500

    Delete weo directory

--- weo/__init__.py
+++
@@ -1,6 +0,0 @@
-from .dataframe import WEODataFrame
-from .download import download
-from .errors import WEO_ParsingError
-from .read import reads_csv
-
-__all__ = ['WEODataFrame', 'download', 'WEO_ParsingError', 'reads_csv']
--- weo/errors.py
+++
@@ -1,3 +0,0 @@
-class WEO_ParsingError(Exception):
-    def __init__(self, iso_code):
-        super().__init__(f"Invalid ISO code provided: {iso_code}")
